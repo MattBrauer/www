d635be979827df0d83a61b450223f58b9f5f0fc8
Matt Brauer <mrauer@gmail.com>
2026-06-06 23:24:52 -0700

    add supabase config

--- willows-sf-tour/config.js
+++ willows-sf-tour/config.js
@@ -1,4 +1,3 @@
 const SUPABASE_URL  = 'https://ccvpkkgjewembxltkyta.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjdnBra2dqZXdlbWJ4bHRreXRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODk3NTksImV4cCI6MjA5NjM2NTc1OX0.Je6J8sANRxtRwIKeRgGBwLoeQdFoW4_1pnftAoIyUm0';
-
 const APP_PASSWORD = 'myeyesareupheresloths';
